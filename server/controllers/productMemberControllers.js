8a03b3f666672d5694eed3e5ef5adf026883847e
krisnawiraska <krisnaswiraska17@gmail.com>
2023-10-28 13:46:43 +0800

    setup controllers product

---
+++ server/controllers/productMemberControllers.js
@@ -0,0 +1,21 @@
+const db = require ("../db")
+
+class ProducControllers {
+    static async getAllDataProduct (req,res){
+        res.send("masuk")
+    }
+    static async getDataProduct (req,res){
+
+    }
+    static async addProduct (req,res){
+
+    }
+    static async editDataProduct (req,re){
+
+    }
+    static async deleteProduct (req,res){
+        
+    }
+}
+
+module.exports = ProducControllers
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const routes = express.Router()
 const userRoutes = require("../routes/userRoutes")
+const productMemberRoutes = require("../routes/productmemberRoutes")
 
 routes.get('/',(req,res)=>{
     res.send("masuk ke route main")
 })
 
 routes.use('/users', userRoutes)
+routes.use('/products', productMemberRoutes)
 module.exports = routes
