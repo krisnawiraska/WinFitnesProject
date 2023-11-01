0f74de0572cba8954c2e8d1e90d83913b62d33aa
krisnawiraska <krisnaswiraska17@gmail.com>
2023-11-01 23:40:36 +0800

    merge

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,14 @@ const routes = express.Router()
 const userRoutes = require("../routes/userRoutes")
 const productMemberRoutes = require("../routes/productmemberRoutes")
 const attRoutes = require("../routes/attandancesRoutes")
+const transactionsRoutes = require("../routes/transactionsRoutes")
 
 routes.get('/',(req,res)=>{
     res.send("masuk ke route main")
 })
 
 routes.use('/users', userRoutes)
-
+routes.use('/transactions', transactionsRoutes)
 routes.use('/products', productMemberRoutes)
 routes.use('/attandance', attRoutes)
 module.exports = routes
